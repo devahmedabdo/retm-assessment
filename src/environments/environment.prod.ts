9cd53733c161d407842140520b8220b2152666b0
Ahmed Abdo <devahmedabdo@gmail.com>
2023-03-16 20:17:29 +0200

    add libraries ,main color ,images ,and set the main font and style

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'https://signup.retm.sa/join/'
 };
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blueColor: "#007aff",
+        darkGreenColor: "#004f2f",
+        midDarkGreenColor: "#008550",
+        lightGreenColor: "#00bc71",
+        grayColor: "#fafafa",
+        darkGrayColor: "#333",
+      },
+    },
+  },
+  plugins: [],
+};
